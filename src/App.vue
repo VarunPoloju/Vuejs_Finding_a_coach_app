cb2a761506bc3bccb642556d0bb8c8e43556e1f7
varun <varunpoloju966@gmail.com>
2024-05-17 16:14:54 +0530

    added page not found comp code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-    <the-header></the-header>
-  <router-view></router-view>
+  <the-header></the-header>
+  <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
 </template>
 
 <script>
-
-import TheHeader from './components/layout/TheHeader.vue'
+import TheHeader from './components/layout/TheHeader.vue';
 
 export default {
   components: { TheHeader },
@@ -25,5 +28,29 @@ html {
 
 body {
   margin: 0;
+}
+
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateY();
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
 }
 </style>
--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-    <div></div>
+  <section>
+    <base-card>
+      <h2>Page Not Found!</h2>
+      <p>
+        This page could not be found, please check out all our
+        <router-link to="/coaches">Coaches</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
